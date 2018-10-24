1f7dcd805274a4b48bf67bdb8d5fb409bf278d8d
Will Douglas <willygdouglas@gmail.com>
2018-10-23 23:13:08 -0600

    Implement mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// High level implementation:
+// i = 1;
+// R2 = 0;
+// while (i <= R1){
+//  R2 += R0;
+//  i++;
+// }
+
+
+    @i
+    M=1 // i=1
+    @R2
+    M=0 // R2=0
+
+// while (i <= R1)
+(LOOP)
+    @i
+    D=M // D=i
+    @R1
+    D=D-M // D=i-R1
+    @END
+    D;JGT // if (i-R1)>0 goto END
+
+    @R0
+    D=M // D=R0
+    @R2
+    M=D+M // R2=R2+R0
+    @i
+    M=M+1
+    @LOOP
+    0;JMP // goto LOOP
+
+(END)
+    @END
+    0;JMP // Infinite loop
